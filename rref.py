bc5ad80ddaba9e2285627784313dc5fd03429be2
Chase Clark <chase_clark07@hotmail.com>
2021-04-07 13:28:09 -0400

    add annotations

--- rref.py
+++ rref.py
@@ -1,12 +1,27 @@
 # import
 import numpy as np
 from fractions import Fraction
-from decimal import Decimal
 import copy
 
-steps_list = []
-matrix = [[2, 0, -6, -8], [0, 1, 2, 3], [3, 6, -2, -4]]
-# matrix = [[1, 2, -3], [3, 0, 5], [4, 2, 3]]
+# TODO
+# print out if the equations are linearly independent or dependent
+# add in user input on startup
+
+
+# input
+# print('enter number number of rows:')
+# rows = int(input())
+# print('enter number number of cols(including right hand side):')
+# cols = int(input())
+
+# for i in range(rows):
+#     print(f'enter row vals separated by spaces for row {i+1}')
+#     r = str(input())
+#     matrix.append(r.split(" "))
+
+
+matrix = [[1, 2, 3, 4], [4, 6, 2, 6], [2, 4, 6, 8]]
+
 
 num_cols = len(matrix[0])
 num_rows = len(matrix)
@@ -18,21 +33,21 @@ def solve():
 
 
 def do_ref():
-    print_matrix()
+    print_matrix('original augmented matrix')
 
-    for r in range(num_rows):  # iterate over the cols - 1
+    for r in range(num_rows):
         # sort the row by putting a non zero entry in the correct position
         if (r < num_rows-1):
             sort_rows(r)
 
-        # check [c][c] needs to be scaled to 1
         leading = matrix[r][r]
         if leading != 0:
             if leading != 1:
                 # divide entire row by leading
                 for c in range(num_cols):
                     matrix[r][c] = float(matrix[r][c]) / leading
-            print_matrix()
+            print_matrix(
+                f'r{r+1} <- r{r+1} / {str(Fraction(leading).limit_denominator(max_denominator=1000))}')
             make_zeroes_below(r, r)
 
     # at this point we are in ref
@@ -50,6 +65,10 @@ def do_rref():
     for r in range(num_rows-1, 0, -1):
         # matrix r r should be a 1 already
         make_zeroes_above(r, r)
+
+
+def check_dependence():
+    pass
 
 
 # this method attempts to place a nonzero leading number
@@ -71,7 +90,7 @@ def swap_rows(index_1, index_2):
     temp = matrix[index_1]
     matrix[index_1] = matrix[index_2]
     matrix[index_2] = temp
-    print_matrix()
+    print_matrix(f'r{index_1+1} <-> r{index_2+1}')
 
 
 def make_zeroes_below(r, c):
@@ -79,31 +98,34 @@ def make_zeroes_below(r, c):
     for i in range(num_rows-r-1):
         current_row = i + r + 1
         scale_amount = float(matrix[current_row][c])
-        for j in range(num_cols):
-            if scale_amount != 0:
+        if scale_amount != 0:
+            for j in range(num_cols):
                 # subtract each value in current row by the scalar amount of the above value
                 matrix[current_row][j] = matrix[current_row][j] - \
                     matrix[r][j] * scale_amount
-        print_matrix()
+            print_matrix(
+                f'r{current_row+1} <- r{current_row+1} - ({str(Fraction(scale_amount).limit_denominator(max_denominator=1000))})r{r+1}')
 
 
 def make_zeroes_above(r, c):
     for i in range(r-1, -1, -1):
         scale_amount = float(matrix[i][c])
-        for j in range(num_cols):
-            if scale_amount != 0:
+        if scale_amount != 0:
+            for j in range(num_cols):
                 # subtract each value in current row by the scalar amount of the above value
                 matrix[i][j] = matrix[i][j] - \
                     matrix[r][j] * scale_amount
-    print_matrix()
+            print_matrix(
+                f'r{i+1} <- r{i+1} - ({str(Fraction(scale_amount).limit_denominator(max_denominator=1000))})r{r+1}')
 
 
 # makes sure all leading values are on the left hand side of the matrix
 def has_solution():
     valid = True
     for r in range(num_rows):
-        if lnz(r) >= num_cols:
-            valid = False
+        if lnz(r) is not None:
+            if lnz(r) >= num_cols:
+                valid = False
     return valid
 
 
@@ -116,8 +138,9 @@ def lnz(row):
 
 
 # converts floats to decimals and uses numpy to format the matrix
-def print_matrix():
+def print_matrix(annotation=''):
     print()
+    print(annotation)
     temp = copy.deepcopy(matrix)
     for i in range(len(temp)):
         for j in range(len(temp[i])):
@@ -127,4 +150,3 @@ def print_matrix():
 
 
 solve()
-# print_matrix()
